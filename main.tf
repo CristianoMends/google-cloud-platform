e5090b746b070e0102e4d3b7dda59f39afbc28b0
Cristiano Mendes <116528159+CristianoMends@users.noreply.github.com>
2025-05-01 17:32:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
 provider "google" {
   project = "<project-ID>"
   region  = "us-central1"
